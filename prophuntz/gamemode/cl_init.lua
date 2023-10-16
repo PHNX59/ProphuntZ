bc05840ce7b0bd096f2805cdcbad108dbcb36e43
Florian_RVD <33377363+Tronpa@users.noreply.github.com>
2023-10-17 01:28:48 +0200

    Update cl_init.lua

--- prophuntz/gamemode/cl_init.lua
+++ prophuntz/gamemode/cl_init.lua
@@ -20,18 +20,16 @@ end)
 net.Receive("HideRoundHUD", function()
     hook.Remove("HUDPaint", "DrawRoundWaitingHUD")
 end)
--- Fonction pour configurer la vue à la troisième personne
 local function SetupThirdPersonView()
     hook.Add("CalcView", "PropHuntThirdPersonView", function(ply, pos, angles, fov)
         local view = {}
-        view.origin = pos - angles:Forward() * 100  -- Ajuster la distance de la caméra derrière le joueur.
+        view.origin = pos - angles:Forward() * 100 
         view.angles = angles
         view.fov = fov
         view.drawviewer = true
         return view
     end)
 end
--- Appeler la fonction pour activer automatiquement la vue à la troisième personne
 SetupThirdPersonView()
 
 
